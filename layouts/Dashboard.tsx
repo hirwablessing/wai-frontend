472b6c044a1d096484eebdc79b7ec87f1cef3741
ishgervais <ishimwegervais11@gmail.com>
2021-06-13 22:37:48 +0200

    added the styled failed to login page

--- layouts/Dashboard.tsx
+++ layouts/Dashboard.tsx
@@ -2,12 +2,19 @@ import Sidebar from "../components/dashboard/sidebar"
 import Navbar from "../components/dashboard/navbar"
 import { useContext } from "react"
 import {UserContext} from '../pages/api/context/UserContext'
+import { LoadingOutlined } from '@ant-design/icons';
 
 export default function Dashboard({children}:any){
     const {checking} = useContext(UserContext)
     console.log("checking ", checking)
     if(checking){
-    return <div>checking</div>
+    return <div className = "h-screen flex items-center justify-center">
+        <div className = "text-center">
+            <LoadingOutlined className="text-5xl my-5 text-blue-700"/>
+            <p>Checking ...</p>
+        </div>
+
+    </div>
     }
     return(
         <div className="block lg:flex gap-7 bg-gray-50 text-sm">
