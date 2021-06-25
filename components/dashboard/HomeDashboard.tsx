3fd07bb0145b646e49ee1287669c2f05686132e8
ishgervais <ishimwegervais11@gmail.com>
2021-06-25 21:48:24 +0200

    added the labels on the message boxes

--- components/dashboard/HomeDashboard.tsx
+++ components/dashboard/HomeDashboard.tsx
@@ -1,6 +1,33 @@
 import DailyVisits from "./charts/DailyVisits";
 
 export default function HomeDashboard(){
+    const data = [
+
+        {
+            firstName:"Ish",
+            lastName:"Gervais",
+            message:"Problems will you start to solve after this registration",
+            date:"04/20/2021"
+        },
+        {
+            firstName:"Ish",
+            lastName:"Gervais",
+            message:"Problems will you start to solve after this registration",
+            date:"04/20/2021"
+        },
+        {
+            firstName:"Ish",
+            lastName:"Gervais",
+            message:"Problems will you start to solve after this registration",
+            date:"04/20/2021"
+        },
+        {
+            firstName:"Ish",
+            lastName:"Gervais",
+            message:"Problems will you start to solve after this registration",
+            date:"04/20/2021"
+        }
+    ];
 return(
 <div className="h-1/2">
     <h1 className="font-bold text-dark-500">Dashboard </h1>
@@ -33,101 +60,29 @@ return(
         <div className="lg:w-1/3" style={{height:'500px'}}>
             <h1 className="font-bold my-4 ml-5">Messages </h1>
             <hr />
+
+            {/* message card */}
             <div className="messages--cards overflow-y-auto px-5">
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
+             
+             {data?.map((item:any)=>{
+                 return(
+                    <div className="card rounded p-3 bg-white my-5 border flex gap-3">
                     <div
                         className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
                         GA</div>
                     <div className="content text-sm">
-                        <h1 className="block my-1 text-gray-500">Ish Gervais</h1>
-                        problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
+                        <h1 className="text-gray-400 mb-2">{item.firstName} {item.lastName}</h1>
+                        <p>{item.message}</p>
+                        <h1
+                            className="text-gray-400 text-xs mt-4 block float-right">{item.date}</h1>
                     </div>
                 </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">
-                        <h1 className="block my-1 text-gray-500">Ish Gervais</h1>
-                        problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">
-                        <h1 className="block my-1 text-gray-500">Ish Gervais</h1>
-                        problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">
-                        <h1 className="block my-1 text-gray-500">Ish Gervais</h1>
-                        problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">
-                        <h1 className="block my-1 text-gray-500">Ish Gervais</h1>
-                        problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">
-                        <h1 className="block my-1 text-gray-500">Ish Gervais</h1>
-                        problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">
-                        <h1 className="block my-1 text-gray-500">Ish Gervais</h1>
-                        problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
-                <div className="card rounded p-5 bg-white my-5 border flex gap-5">
-                    <div
-                        className="rounded-lg p-5 h-1/2 text-center bg-blue-100 text-dark flex items-center font-bold justify-center">
-                        GA</div>
-                    <div className="content text-sm">problems will you start to solve after this registration <h1
-                            className="text-gray-400 text-sm mt-4 block float-right">04/20/2021</h1>
-                    </div>
-                </div>
+                 )
+             })}
+
+               
+
+
             </div>
         </div>
 
